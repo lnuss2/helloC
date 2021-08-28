82e6672d9eba7024384aa2117e93acd92f180d83
lnuss2 <lnuss2@huskers.unl.edu>
2021-08-28 14:18:48 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Landon Nuss
+ *         lnuss2@huskers.unl.edu
+ * Date: 2021/09/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Landon Nuss!\n");
+  /printf("Computer Science\n");
+
+  return 0;
+}
